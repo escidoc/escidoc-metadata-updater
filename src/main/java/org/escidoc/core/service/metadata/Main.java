9b5a4ca56710304eff832607fe6df58c5162a42d
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2012-01-16 15:22:23 +0100

    rename to Main

--- src/main/java/org/escidoc/core/service/metadata/Main.java
+++ src/main/java/org/escidoc/core/service/metadata/Main.java
@@ -7,7 +7,7 @@ import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.escidoc.core.service.metadata.guice.AppServletConfig;
 
-public class AppLauncher {
+public class Main {
 
   private static final int PORT_NUMBER = 8088;
 
